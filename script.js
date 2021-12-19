98c89cfc9bdc3776288ff26d632da70e55443b74
Androgem <96374015+Androgem@users.noreply.github.com>
2021-12-19 12:03:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert('Calculate the Area of Circle');
+console.log('Area of Circle');
+let pi = 3.14159265359;
+let r = window.prompt('Input your radius value');
+let AreaOfCircle = pi*(r*r);
+alert('The area of Circle is '+ AreaOfCircle);
+console.log('The area of Circle is '+ AreaOfCircle);
