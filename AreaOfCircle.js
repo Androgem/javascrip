77ef98dfc388d19da01d4a7c320fc70b6b9edce6
Androgem <96374015+Androgem@users.noreply.github.com>
2021-12-21 20:41:11 +0100

    Update AreaOfCircle.js

--- AreaOfCircle.js
+++ AreaOfCircle.js
@@ -1,7 +1,10 @@
 alert('Calculate the Area of Circle');
 console.log('Area of Circle');
 let pi = 3.14159265359;
-let r = window.prompt('Input your radius value');
+let input = window.prompt('Input your radius value');
+let r = parseInt(input)
 let AreaOfCircle = pi*(r*r);
-alert('The area of Circle is '+ AreaOfCircle);
+     if ( isNaN(r)) {
+              alert('Write a number!')}
+     else    {alert('The area of Circle is '+ AreaOfCircle)};
 console.log('The area of Circle is '+ AreaOfCircle);
