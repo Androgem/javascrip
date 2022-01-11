7d959233d24ed5023f53cdfbcf405f8717eeb379
Androgem <96374015+Androgem@users.noreply.github.com>
2022-01-11 20:07:22 +0100

    Add files via upload

---
+++ tellStory.js
@@ -0,0 +1,12 @@
+function tellStory (nameMoodActivity) {
+  let story = alert(`Hello my name is ${nameMoodActivity[0]}, i am ${nameMoodActivity[1]} today and my activity for today is ${nameMoodActivity[2]}`)
+  console.log(nameMoodActivity);
+  return story
+}
+
+let nameMoodActivity = [];
+nameMoodActivity[0] = prompt('Write your name');
+nameMoodActivity[1] = prompt('Write your mood for today');
+nameMoodActivity[2] = prompt('Write what is your activity for today');
+
+tellStory(nameMoodActivity);
